3ff6a5826dcebeb89a514b98688e0b37fd348192
Jiu <99115192+JiuGeFaCai@users.noreply.github.com>
2026-09-04 20:56:01 +0800

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Wild Plus Kernel by TheWildJames or Morgan Weedman
+kernel.string=Android Kernel by 闲鱼搞机英雄
 do.devicecheck=0
 do.modules=0
 do.systemless=0
